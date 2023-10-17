c9850f1ddc5778d32fcf739e8f811d359c585427
Caio Ferraz <caioferrazalmeida.27@gmail.com>
2023-10-17 16:05:28 -0300

    feat: starting with TS

---
+++ angular/TypeScript/build/index.js
@@ -0,0 +1,22 @@
+"use strict";
+let number = 1;
+let string = "1";
+let bool = true;
+let float = 1.9;
+let nulo = null;
+let indefinido = undefined;
+let vazio;
+let x = "any";
+let objeto = {
+    name: "Hulk",
+    vulgo: "Hulk Smash",
+};
+const heroi = { name: "Hulk", vulgo: "Hulk Smash" };
+console.log(heroi);
+let array = ["x", "y"];
+let array1 = ["x", 2, false];
+let array2 = ["x", "y"];
+let array3 = ["x", 2];
+let tuplas = ["x", 2, 3];
+let data = new Date();
+console.log(data.toString());
