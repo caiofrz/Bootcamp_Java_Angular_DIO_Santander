d05fcdebcab4e9025eb84ee09700b5746fc19dcd
Caio Ferraz <caioferrazalmeida.27@gmail.com>
2023-10-18 13:25:54 -0300

    feat: continuing with TS

--- angular/TypeScript/src/index.ts
+++ angular/TypeScript/src/index.ts
@@ -19,8 +19,8 @@ type Hero = {
   vulgo: string;
 };
 
-const heroi: Hero = { name: "Hulk", vulgo: "Hulk Smash" };
-console.log(heroi);
+const heroi1: Hero = { name: "Hulk", vulgo: "Hulk Smash" };
+console.log(heroi1);
 
 const array: string[] = ["x", "y"];
 const array1: any[] = ["x", 2, false];
@@ -38,3 +38,53 @@ function somar(x: number, y: number) : number{
 
 // const soma: string = somar(1, 2); // n consegue
 const soma: number = somar(1, 2);
+
+//
+//
+//INTERFACE
+//
+//
+interface IHero {
+  readonly name: string;
+  vulgo: string;
+}
+
+const heroi: IHero = { name: "Hulk", vulgo: "Hulk Smash" };
+console.log(heroi);
+
+interface robot {
+  id: number | string;
+  name: string;
+}
+
+class Bot implements robot{
+    id: string | number;
+    name: string;
+    
+    constructor(id: string | number, name: string) {
+        this.id = id;
+        this.name = name;
+    }
+    hello() : void {
+        console.log("hello");
+    }
+
+}
+
+const BOT = new Bot(1, "caio");
+console.log(BOT.hello());
+
+
+//
+//
+//GENERICS
+//
+//
+function concatArray<T>(...itens: T[]) : T[]{
+  return new Array().concat(...itens);
+}
+
+const numArray = concatArray<number[]>([1, 2], [3]);
+const stgArray = concatArray<string[]>(["aa", "b"], ["c"]);
+
+
