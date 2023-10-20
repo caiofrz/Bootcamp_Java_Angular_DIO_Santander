68c6a0651ada23acbac39d4cefcaea206c77029c
Caio Ferraz <caioferrazalmeida.27@gmail.com>
2023-10-20 12:33:51 -0300

    feat: working with ngDiretivas and Modules

--- angular/hello-angular/src/app/app.module.ts
+++ angular/hello-angular/src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TitleComponent } from './title/title.component';
 import { CardComponent } from './card/card.component';
+import { CompAtributosComponent } from './comp-atributos/comp-atributos.component';
+import { ListaModule } from './lista/lista.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     TitleComponent,
-    CardComponent
+    CardComponent,
+    CompAtributosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+		FormsModule,
+		ListaModule
   ],
   providers: [],
   bootstrap: [AppComponent]
